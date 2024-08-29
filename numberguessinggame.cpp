66a61a25fe36ee3f78e8050d2c362697f2e3e7d2
Sharath H N <ghost@Sharaths-MacBook-Air.local>
2024-08-29 21:23:21 +0530

    number game file added

--- numberguessinggame.cpp
+++ numberguessinggame.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <cstdlib>  
+
+using namespace std;
+int main() {
+   
+    std::srand(std::time(0)); 
+    
+    // Generate a random number between 1 and 100
+    int randomNumber = std::rand() % 100 + 1;
+    int userGuess = 0;
+    
+    cout << "Welcome to the Number Guessing Game!" << std::endl;
+    cout << "I have selected a number between 1 and 100." << std::endl;
+    std::cout << "Can you guess what it is?" << std::endl;
+
+    // Loop until the user guesses the correct number
+    while (userGuess != randomNumber) {
+        std::cout << "Enter your guess: ";
+        std::cin >> userGuess;
+        
+        // Provide feedback based on the user's guess
+        if (userGuess < randomNumber) {
+            std::cout << "Too low! Try again." << std::endl;
+        } else if (userGuess > randomNumber) {
+            std::cout << "Too high! Try again." << std::endl;
+        } else {
+            std::cout << "Congratulations! You guessed the number!" << std::endl;
+        }
+    }
+
+    return 0;
+}
 
